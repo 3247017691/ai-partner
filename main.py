93c53c9b0942e7c80db1f1de7dc6df858d38dbd0
xie123 <3247017691@qq.com>
2026-08-11 10:17:22 +0800

    提交

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# ————————常量—————————————
+PRESET_FILE_PATH = "data/companion_presets.json"
 
-@app.get("/")
-async def root():
+
+@app.get("/api/presets", summary='取得所有AI伴侣预设数据，前端用于渲染选择下拉框')
+async def presets():
+
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+
+
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='0.0.0.0', port=8000,)
